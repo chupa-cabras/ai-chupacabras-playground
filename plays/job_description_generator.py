c2b4e9224f41100015adf9de6cb090583c590a16
Chupacabras <dayvison.w.pellegrina@gmail.com>
2024-06-27 14:49:12 -0300

    Fix plays and creates factory

--- plays/job_description_generator.py
+++ plays/job_description_generator.py
@@ -13,38 +13,29 @@ with get_openai_callback() as cb:
     print(f"Total Cost (USD): ${cb.total_cost}")
 
 
-#Instance Agents 
+# Create a researcher agent
 talent_acquisition_specialist_agent = Agent(
-    role='Talent Acquisition Specialist',
-    goal='Provide best job descriptions to attract top talent.',
-    backstory='A Talent Acquisition Specialist finds, attracts, and recruits the best talent for a company.',
-    llm_model=azure_llm,
-    tools=[],
-    function_calling_llm=azure_llm,  # Optional
-    max_iter=15,  # Optional
-    max_rpm=None, # Optional
-    verbose=True,  # Optional
-    allow_delegation=True,  # Optional
-    #step_callback=my_intermediate_step_callback,  # Optional
-    cache=True  # Optional
+  role='Talent Acquisition Specialist',
+  goal='Provide best job descriptions to attract top talent.',
+  backstory='A Talent Acquisition Specialist finds, attracts, and recruits the best talent for a company.',
+  verbose=True,
+  llm=azure_llm,
+  function_calling_llm=azure_llm
 )
 
-#Instance Tasks
+# #Instance Tasks
 task = Task(
     description='Write the best job description for a Software Enginnering Manager role.',
     expected_output='A resume with main bullet points',
-    agent=talent_acquisition_specialist_agent,
-    output_file='job_description.md'  # The final blog post will be saved here
+    agent=talent_acquisition_specialist_agent
 )
 
-#Instance Crew
+# #Instance Crew
 crew = Crew(
     agents=[talent_acquisition_specialist_agent],
     tasks=[task],
     verbose=2
 )
 
-# Execute tasks
+# # Execute tasks
 crew.kickoff()
-
-
