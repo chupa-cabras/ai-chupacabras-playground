295234dd985681abeefb8ee41e2df7132ef55fe2
Chupacabras <dayvison.w.pellegrina@gmail.com>
2024-08-02 17:39:37 -0300

    Rag Example

--- models/factory.py
+++ models/factory.py
@@ -1,21 +1,25 @@
 import os
-from langchain_openai import AzureChatOpenAI
 import google.generativeai as genai
 
+from chromadb import Embeddings
+from langchain_openai import AzureChatOpenAI, AzureOpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
+from dotenv import load_dotenv
 from models.llm_settings import LLM_Settings
 
 class Factory:
   
     def create(self, model_name):
+        load_dotenv()
+
         if model_name == 'gpt_azure_4o':
             azure_llm_settings = LLM_Settings().instance_model_settings(model_name)
 
-            #Itś necessary for the lib
+            #It's necessary for the lib
             os.environ["AZURE_OPENAI_VERSION"] = azure_llm_settings.api_version
             os.environ["AZURE_OPENAI_DEPLOYMENT"] = azure_llm_settings.deployment
             os.environ["AZURE_OPENAI_ENDPOINT"] = azure_llm_settings.endpoint
             os.environ["AZURE_OPENAI_KEY"] = azure_llm_settings.key
-
 
             model = AzureChatOpenAI(
                 openai_api_version = azure_llm_settings.api_version,
@@ -26,8 +30,8 @@ class Factory:
             )
 
             return model
+        if model_name == 'openai_text_embedding_3_small':
+            llm_settings = LLM_Settings().instance_model_settings(model_name)
+            return OpenAIEmbeddings(openai_api_key = llm_settings.key, model = 'text-embedding-3-small')
+
         return None
-        
-
-
-
